4f002b0aeb59c39176d65329780d621eac536857
FlykeSpice <flykespice@gmail.com>
2025-07-28 18:35:19 -0400

    Lower the api level to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.flykespice.povray"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 35
         versionCode = 1
         versionName = "0.1"
